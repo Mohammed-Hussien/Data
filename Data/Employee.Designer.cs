1b364299ff419f3c8786794dd5f27fcdd03d03e6
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 22:41:41 +0200

    change pos

--- Data/Employee.Designer.cs
+++ Data/Employee.Designer.cs
@@ -38,7 +38,7 @@
             this.panel1.ForeColor = System.Drawing.Color.Teal;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1913, 100);
+            this.panel1.Size = new System.Drawing.Size(1913, 133);
             this.panel1.TabIndex = 0;
             // 
             // Employee
