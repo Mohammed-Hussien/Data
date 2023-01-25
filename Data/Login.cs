d177286574d67cafd0c61c2e6e9c7a38e02c144c
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 19:35:08 +0200

    add panel and change his color

--- Data/Login.cs
+++ Data/Login.cs
@@ -2,6 +2,8 @@ namespace Data
 {
     public partial class Form1 : Form
     {
+        private Panel panel2;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,12 +11,22 @@ namespace Data
 
         private void InitializeComponent()
         {
+            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Location = new System.Drawing.Point(28, 110);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 1;
             // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(591, 548);
+            this.Controls.Add(this.panel2);
             this.Name = "Form1";
             this.ResumeLayout(false);
 
