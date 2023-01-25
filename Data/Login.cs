3fe4b6a14b4bb38295b223c8aff9954dfc566914
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 19:33:28 +0200

    change size

--- Data/Login.cs
+++ Data/Login.cs
@@ -6,5 +6,17 @@ namespace Data
         {
             InitializeComponent();
         }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(591, 548);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
     }
 }
