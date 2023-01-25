4f73e8bf5666bad211eb246e9e929a255e9f20f3
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 22:38:45 +0200

    change size

--- Data/Employee.Designer.cs
+++ Data/Employee.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 450);
+            this.ClientSize = new System.Drawing.Size(348, 293);
             this.Name = "Employee";
             this.Text = "Employee";
             this.ResumeLayout(false);
