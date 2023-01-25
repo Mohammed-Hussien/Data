dabf9cb501d998b616f209af9fb1e4ed834c33db
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 22:44:59 +0200

    change text

--- Data/Employee.Designer.cs
+++ Data/Employee.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,12 +48,24 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(735, 71);
+            this.label1.Location = new System.Drawing.Point(709, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(233, 21);
+            this.label1.Size = new System.Drawing.Size(383, 24);
             this.label1.TabIndex = 1;
-            this.label1.Text = " Employee Management System";
+            this.label1.Text = " Employee Management System Version 1.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(709, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Manage Employee";
             // 
             // Employee
             // 
@@ -60,6 +73,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1913, 1147);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -69,6 +83,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,5 +91,6 @@
 
         private Panel panel1;
         private Label label1;
+        private Label label2;
     }
 }
