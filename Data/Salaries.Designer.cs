f65008b2a49d81c956c1f2c9c1124993e0604a0c
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 23:14:52 +0200

    remove

--- Data/Salaries.Designer.cs
+++ Data/Salaries.Designer.cs
@@ -33,10 +33,6 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -99,42 +95,6 @@
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(1913, 56);
             this.flowLayoutPanel1.TabIndex = 36;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.ForeColor = System.Drawing.Color.Teal;
-            this.label7.Location = new System.Drawing.Point(14, 832);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(121, 15);
-            this.label7.TabIndex = 35;
-            this.label7.Text = "Employee Daily salary";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.White;
-            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label8.ForeColor = System.Drawing.Color.Teal;
-            this.label8.Location = new System.Drawing.Point(14, 746);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(72, 21);
-            this.label8.TabIndex = 34;
-            this.label8.Text = "Join date";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(12, 857);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(207, 23);
-            this.textBox5.TabIndex = 32;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(12, 784);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(207, 23);
-            this.textBox6.TabIndex = 33;
             // 
             // label6
             // 
@@ -253,10 +213,6 @@
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
@@ -283,10 +239,6 @@
         private ComboBox comboBox2;
         private PictureBox pictureBox1;
         private FlowLayoutPanel flowLayoutPanel1;
-        private Label label7;
-        private Label label8;
-        private TextBox textBox5;
-        private TextBox textBox6;
         private Label label6;
         private TextBox textBox1;
         private Label label4;
