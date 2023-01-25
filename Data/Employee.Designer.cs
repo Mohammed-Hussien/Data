1f54b9b19cf81e6ced1166c70998a621772e2faa
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 22:39:17 +0200

    remove border

--- Data/Employee.Designer.cs
+++ Data/Employee.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(348, 293);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Employee";
             this.Text = "Employee";
             this.ResumeLayout(false);
