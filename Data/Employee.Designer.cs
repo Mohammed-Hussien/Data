40bc56e5f9bce3cc8ae2e6c4ab2f2d3945f840bb
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 22:41:27 +0200

    change position

--- Data/Employee.Designer.cs
+++ Data/Employee.Designer.cs
@@ -34,10 +34,11 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.ForeColor = System.Drawing.Color.Teal;
-            this.panel1.Location = new System.Drawing.Point(643, 131);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(1913, 100);
             this.panel1.TabIndex = 0;
             // 
             // Employee
