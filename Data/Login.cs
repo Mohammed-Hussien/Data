8183641bb62528f133dad3c6530ca5a137984d38
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 19:37:35 +0200

    change color for label

--- Data/Login.cs
+++ Data/Login.cs
@@ -28,11 +28,13 @@ namespace Data
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(385, 91);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
@@ -43,6 +45,11 @@ namespace Data
             this.Name = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
 
         }
     }
