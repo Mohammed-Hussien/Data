977f6cf9e71d9368b0b42aafbb6079b69f197180
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 19:36:58 +0200

    add label

--- Data/Login.cs
+++ Data/Login.cs
@@ -2,6 +2,7 @@ namespace Data
 {
     public partial class Form1 : Form
     {
+        private Label label1;
         private Panel panel2;
 
         public Form1()
@@ -12,6 +13,7 @@ namespace Data
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel2
@@ -23,13 +25,24 @@ namespace Data
             this.panel2.Size = new System.Drawing.Size(200, 548);
             this.panel2.TabIndex = 1;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(591, 548);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel2);
             this.Name = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
     }
