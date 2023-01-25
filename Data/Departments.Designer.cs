a9c2b41349b7b2477ec1a49463668a94bd05a6ad
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-25 23:01:08 +0200

    change size

--- Data/Departments.Designer.cs
+++ Data/Departments.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(323, 450);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Departments";
             this.Text = "Departments";
