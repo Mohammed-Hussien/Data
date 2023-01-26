5c93f1233602fcfd2bc53bbf4ce80a356c826a38
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:05:08 +0200

    add query

--- Data/Departments.cs
+++ Data/Departments.cs
@@ -37,6 +37,7 @@ namespace EmployeeMgmt1
             {
                 string Dep = DepNameTb.Text;
                 string Query = "insert into DepartmentTb1 values('{0}')";
+                Query = string.Format(Query, DepNameTb.Text);
 
 
         
