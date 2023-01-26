157db00b4865bb9e81af440d132aa431d065a904
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:19:23 +0200

    add salary query

--- Data/Employee.cs
+++ Data/Employee.cs
@@ -82,3 +82,6 @@ namespace EmployeeMgmt1
                     int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
                     string DOB = DOBTb.Value.ToString();
                     string JDate = JDateTb.Value.ToString();
+                    int Salary = Convert.ToInt32(DailySalTb.Text);
+                    string Query = "insert into EmployeeTb1 values('{0}','{1}',{2},'{3}','{4}',{5})";
+                    Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary);
