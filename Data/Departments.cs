f0ebe84ade2c0b36c066b655c23f390602d5ff68
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:01:30 +0200

    add function

--- Data/Departments.cs
+++ Data/Departments.cs
@@ -22,8 +22,7 @@ namespace EmployeeMgmt1
     }
     private void ShowDepartments()
     {
-        string Query = "Select * from DepartmentTb1";
-        DepList.DataSource = Con.GetData(Query);
+       
     }
 
 
