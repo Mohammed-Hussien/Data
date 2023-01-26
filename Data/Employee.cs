f0be89cd0b0ca99f8cfd41b7a7911bd12ce6907d
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:14:29 +0200

    add library

--- Data/Employee.cs
+++ Data/Employee.cs
@@ -10,31 +10,3 @@ using System.Windows.Forms;
 
 namespace EmployeeMgmt1
 {
-    public partial class Employees : Form
-    {
-        Functions Con;
-        public Employees()
-        {
-            InitializeComponent();
-            Con = new Functions();
-            ShowEmp();
-            GetDepartment();
-        }
-        private void ShowEmp()
-        {
-            try
-            {
-                string Query = "Select * from EmployeeTb1";
-                EmployeeList.DataSource = Con.GetData(Query);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
