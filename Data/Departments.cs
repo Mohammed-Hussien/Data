c238f39fa5a9a05feac571aa35387e87045e029c
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 18:59:57 +0200

    add function

--- Data/Departments.cs
+++ Data/Departments.cs
@@ -10,115 +10,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EmployeeMgmt1
+   public partial class Departments : Form
 {
-    public partial class Departments : Form
+    Functions Con;
+    public Departments()
     {
-        Functions Con;
-        public Departments()
-        {
-            InitializeComponent();
-            Con = new Functions();
-            ShowDepartments();
-        }
-        private void ShowDepartments()
-        {
-            string Query = "Select * from DepartmentTb1";
-            DepList.DataSource = Con.GetData(Query);
-        }
-
-        private void AddBtn_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (DepNameTb.Text == "")
-                {
-                    MessageBox.Show("missing data!!!");
-                }
-                else
-                {
-                    string Dep = DepNameTb.Text;
-                    string Query = "insert into DepartmentTb1 values('{0}')";
-                    Query = string.Format(Query, DepNameTb.Text);
-                    Con.SetData(Query);
-                    ShowDepartments();
-                    MessageBox.Show("Department Added!!!");
-                    DepNameTb.Text = "";
-                }
-            }
-            catch (Exception Ex)
-            {
-                MessageBox.Show(Ex.Message);
-            }
-        }
-        int key = 0;
-        private void DepList_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            DepNameTb.Text = DepList.SelectedRows[0].Cells[0].Value.ToString();
-            if (DepNameTb.Text == "")
-            {
-                key = 0;
-            }
-            else
-            {
-                key = Convert.ToInt32(DepList.SelectedRows[0].Cells[0].Value.ToString());
-            }
-        }
-
-        private void EditBtn_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (DepNameTb.Text == "")
-                {
-                    MessageBox.Show("missing data!!!");
-                }
-                else
-                {
-                    string Dep = DepNameTb.Text;
-                    string Query = "Update DepartmentTb1 set Depname = '{0}' where Depid = {1}";
-                    Query = string.Format(Query, DepNameTb.Text, key);
-                    Con.SetData(Query);
-                    ShowDepartments();
-                    MessageBox.Show("Department Updated!!!");
-                    DepNameTb.Text = "";
-                }
-            }
-            catch (Exception Ex)
-            {
-                MessageBox.Show(Ex.Message);
-            }
-        }
-
-        private void DeleteBtn_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (DepNameTb.Text == "")
-                {
-                    MessageBox.Show("missing data!!!");
-                }
-                else
-                {
-                    string Dep = DepNameTb.Text;
-                    string Query = "Delete from DepartmentTb1 where Depid = {0}";
-                    Query = string.Format(Query, key);
-                    Con.SetData(Query);
-                    ShowDepartments();
-                    MessageBox.Show("Department Deleted!!!");
-                    DepNameTb.Text = "";
-                }
-            }
-            catch (Exception Ex)
-            {
-                MessageBox.Show(Ex.Message);
-            }
-        }
-
-        private void EmpLbl_Click(object sender, EventArgs e)
-        {
-            Employees obj = new Employees();
-            obj.Show();
-            this.Hide();
-        }
+        InitializeComponent();
+        Con = new Functions();
+        ShowDepartments();
     }
-}
+    private void ShowDepartments()
+    {
+        string Query = "Select * from DepartmentTb1";
+        DepList.DataSource = Con.GetData(Query);
+    }
+
+
+        
