902806cb1bb64c7b282b735c411818f3ddeede7e
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:04:58 +0200

    add srting query

--- Data/Departments.cs
+++ Data/Departments.cs
@@ -36,5 +36,7 @@ namespace EmployeeMgmt1
             else
             {
                 string Dep = DepNameTb.Text;
+                string Query = "insert into DepartmentTb1 values('{0}')";
+
 
         
