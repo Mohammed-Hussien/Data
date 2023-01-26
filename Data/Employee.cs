5d4d571f14bf39e2e309cbaa822ca2d9413ff181
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:15:42 +0200

    add string query

--- Data/Employee.cs
+++ Data/Employee.cs
@@ -24,3 +24,5 @@ namespace EmployeeMgmt1
         {
             try
             {
+                string Query = "Select * from EmployeeTb1";
+                EmployeeList.DataSource = Con.GetData(Query);
