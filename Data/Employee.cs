2744ea71a42fa5488f0e29531a5cf4ae8de27ebe
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:22:10 +0200

    add massagebox

--- Data/Employee.cs
+++ Data/Employee.cs
@@ -136,3 +136,6 @@ namespace EmployeeMgmt1
             try
             {
                 if (EmpNameTb.Text == "" || GenCb.SelectedIndex == -1 || DepCb.SelectedIndex == -1 || DailySalTb.Text == "")
+                {
+                    MessageBox.Show("missing data!!!");
+                }
