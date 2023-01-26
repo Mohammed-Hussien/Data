b13c87f1b49dd2d5f083592cd6d9f59a94b7eb7d
Mohamed-Hussien <hidarm602@gmail.com>
2023-01-26 19:22:48 +0200

    add massagebox

--- Data/Employee.cs
+++ Data/Employee.cs
@@ -147,3 +147,14 @@ namespace EmployeeMgmt1
                     string DOB = DOBTb.Value.Date.ToString();
                     string JDate = JDateTb.Value.Date.ToString();
                     int Salary = Convert.ToInt32(DailySalTb.Text);
+                    string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
+                    Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
+                    Con.SetData(Query);
+                    ShowEmp();
+                    MessageBox.Show("Emoloyee Updated!!!");
+                    EmpNameTb.Text = "";
+                    DailySalTb.Text = "";
+                    GenCb.SelectedIndex = -1;
+                    DepCb.SelectedIndex = -1;
+                }
+            }
